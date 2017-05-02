d6bc3f3f1dad2f0e48619c92a99c2d61e84c1233
stedyyulius <stedyyulius@gmail.com>
2017-05-02 17:36:04 +0700

    fixed

--- parser.js
+++ parser.js
@@ -20,24 +20,17 @@ class PersonParser {
   }
 
   get people() {
-    let file = fs.readFileSync(this._file,"utf-8").split(`\n`)
+    let file = fs.readFileSync(this._file,"utf-8").toString().split(`\n`)
     let splitdata;
     for (let i = 0; i < file.length; i++){
       splitdata = file[i].split(',')
-      this._people.push(new Person(
-      splitdata[0],
-      splitdata[1],
-      splitdata[2],
-      splitdata[3],
-      splitdata[4],
-      splitdata[5]
-    ))
+      this._people.push(new Person(splitdata[0],splitdata[1],splitdata[2],splitdata[3],splitdata[4],splitdata[5]))
     }
-    return this._people.join('\n')
+    return this._people
   }
 
   get size(){
-    return this._people.length -2
+    return this._people.length -1
   }
 
   get file(){
@@ -48,21 +41,24 @@ class PersonParser {
     // let newPerson = new Person(ObjectPerson)
     this._people.push(ObjectPerson)
     console.log(`${ObjectPerson.first_name} Added!`)
-    return this._people.join('\n')
+    return this._people
   }
 
   save(){
-    let newinput =[
-    JSON.stringify(this._people[this._people.length-1].id),
-    JSON.stringify(this._people[this._people.length-1].first_name),
-    JSON.stringify(this._people[this._people.length-1].last_name),
-    JSON.stringify(this._people[this._people.length-1].email),
-    JSON.stringify(this._people[this._people.length-1].phone),
-    JSON.stringify(this._people[this._people.length-1].createdAt)]
+    let temp = []
+    // for(let i = 0; i < this._people.length; i++){
+    let newinput =
+    `${this._people[this._people.length-1].id},${this._people[this._people.length-1].first_name},${this._people[this._people.length-1].last_name},${this._people[this._people.length-1].email},${this._people[this._people.length-1].phone},${this._people[this._people.length-1].createdAt}`
+    // `${this._people[i].id},${this._people[i].first_name},${this._people[i].last_name},${this._people[i].email},${this._people[i].phone},${this._people[i].createdAt}`
+    temp.push("\n" + newinput)
+  //}
 
-    fs.appendFileSync(this._file,newinput)
+    fs.appendFileSync(this._file,temp)
     console.log(`${this._people[this._people.length-1].first_name} Saved!`)
-    return this._people
+  }
+
+  reset(){
+    this._people = []
   }
 
 }
@@ -77,9 +73,13 @@ class PersonParser {
 // }
 
 let parser = new PersonParser('people.csv')
-parser.people
-parser.addPerson(new Person(201,'stedy','yulius','stedyyulius@gmail.com',087878559222,new Date().toISOString()))
+ parser.people
+// parser.addPerson(new Person(201,'stedy','yulius','stedyyulius@gmail.com',087878559222,new Date().toISOString()))
+// parser.save()
+// // parser.reset()
+// // parser.people
+parser.addPerson(new Person(202,'recca','lie','recca_lie@gmail.com',08735842234,new Date().toISOString()))
 parser.save()
-// parser.addPerson(new Person(202,'recca','lie','recca_lie@gmail.com',08735842234,new Date().toISOString()))
+// parser.addPerson(new Person(203,'james','sraun','james_sraun@gmail.com',08623472174,new Date().toISOString()))
 // parser.save()
 console.log(`There are ${parser.size} people in the file '${parser.file}'.`)
