bc1a359ebab8006e2abdb364c32bf2ed22a72665
aldyadk <aldy.andika@gmail.com>
2017-05-02 17:55:33 +0700

    done

--- parser.js
+++ parser.js
@@ -50,25 +50,24 @@ class PersonParser {
     this._people.push(person);
   }
   save(){
-    //let temp = fs.writeFileSync(this._file,JSON.stringify(this._people));
-    //let temp = fs.writeFileSync(this._file,this._people.stringified());
-    let arr= [];
-    for (var i = 0; i < this._people.length; i++) {
-      let temp =`${this._people[i].id},${this._people[i].firstName},${this._people[i].lastName},${this._people[i].email},${this._people[i].phone},${this._people[i].createdAt}`
-      arr.push(temp)
-    }
-    console.log(arr);
-    let temp = fs.writeFileSync(this._file,arr.join('\n'));
+    //=========CARA 1 KLO PAKE writeFileSync==============
+    // let arr= [];
+    // for (var i = 0; i < this._people.length; i++) {
+    //   let temp =`${this._people[i].id},${this._people[i].firstName},${this._people[i].lastName},${this._people[i].email},${this._people[i].phone},${this._people[i].createdAt}`
+    //   arr.push(temp)
+    // }
+    // console.log(arr);
+    // let temp = fs.writeFileSync(this._file,arr.join('\n'));
+
+    //=========CARA 2 KLO PAKE append==============
+    let temp =`\n${this._people[this.size-1].id},${this._people[this.size-1].firstName},${this._people[this.size-1].lastName},${this._people[this.size-1].email},${this._people[this.size-1].phone},${this._people[this.size-1].createdAt}`
+    let asd = fs.appendFileSync(this._file,temp)
   }
 }
 
 let parser = new PersonParser('people.csv');
 console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
-//console.log(parser._people);
-//console.log(parser.people);
 let aldy = (['201','Aldy','Andika','aldy.andika@gmail.com','0809183742',new Date()])
 parser.addPerson(aldy);
 console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
-console.log(parser._people[parser._people.length-3],parser._people[parser._people.length-2],parser._people[parser._people.length-1])//,arr[arr.length-1]);
-// console.log(`Now, There are ${parser.people.size} people in the file '${parser.file}'.`)
 parser.save();
