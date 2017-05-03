503ebcac8ccf9333f775138fdc3b6e2253d1b275
DeriKurniawan <derikurniawan11d88@gmail.com>
2017-05-03 11:54:19 +0700

    creating code

--- parser.js
+++ parser.js
@@ -1,25 +1,78 @@
 "use strict"
-
+const fs = require('fs')
 class Person {
-  // Look at the above CSV file
-  // What attributes should a Person object have?
+    // Look at the above CSV file
+    // What attributes should a Person object have?
+    constructor(obj) {
+        this.id = obj['id'];
+        this.firstName = obj['firstName'];
+        this.lastName = obj['lastName'];
+        this.email = obj['email'];
+        this.phone = obj['phone'];
+        this.createdAt = obj['createdAt'];
+    }
+
 }
 
 class PersonParser {
 
-  constructor(file) {
-    this._file = file
-    this._people = null
-  }
+    constructor(file) {
+        this._file = file
+        this._people = [];
+    }
 
-  get people() {
-    return this._people
-  }
+    get people() {
+        let data = [];
+        let file = fs.readFileSync(this._file, 'utf-8').split('\n');
 
-  addPerson() {}
+        for (let i = 0; i < file.length; i++) {
+            data[i] = file[i].split(',');
+        }
+        for (let i = 1; i < file.length; i++) {
+            let date = new Date(data[i][5]);
+            let person = new Person({
+                id: data[i][0],
+                firstName: data[i][1],
+                lastName: data[i][2],
+                email: data[i][3],
+                phone: data[i][4],
+                createdAt: data[i][5]
+            });
+            this._people.push(person);
+        }
+
+        return this._people;
+    }
+
+    get size() {
+        return this._people.length - 1;
+
+    }
+
+    addPerson(obj) {
+        this._people.push(obj);
+        console.log(this._people[201]);
+    }
+
+    save() {
+        let tempInd = this._people.length - 1;
+        let str = `${this._people[tempInd].id},${this._people[tempInd].firstName},${this._people[tempInd].lastName},${this._people[tempInd].email},${this._people[tempInd].phone},${this._people[tempInd].createdAt}\n`;
+        fs.appendFileSync('people.csv', str);
+    }
 
 }
 
-let parser = new PersonParser('people.csv')
-
-console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
+let parser = new PersonParser('people.csv');
+console.log(parser.people);
+console.log(`There are ${parser.size} people in the file '${parser._file}'.`)
+parser.addPerson(new Person({
+    id: '201',
+    firstName: 'Deri',
+    lastName: 'Kurniawan',
+    email: 'deri@mail.com',
+    phone: '0327327238',
+    createdAt: '2017-04-22:09:03-08:00'
+}));
+parser.save();
+console.log(parser.people);
+console.log(`There are ${parser.size} people in the file '${parser._file}'.`)
